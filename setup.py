feca301c5b37b68ffbe306f2e75d6f45b5a7ace8
Andrew Baisley <andrew.baisley@gmail.com>
2021-03-17 17:53:35 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,21 +9,12 @@ setup(
     # Needed to actually package something
     packages=['neubase'],
     # Needed for dependencies
-    install_requires=[
-        'numpy',
-        'sqlite3',
-        'os',
-        'pandas',
-        're',
-        'json',
-        'datetime',
-        'tarfile',
-    ],
+    install_requires=['pandas'],
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
     license='MIT',
-    description='A wrapper for SQLite databases intended to store DfE data tables.',
+    description='A wrapper for SQLite databases and Pandas DataFrames intended to store DfE data tables.',
     # We will also need a readme eventually (there will be a warning)
     # long_description=open('README.txt').read(),
 )
